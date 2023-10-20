94f6332e1ff9c9dc7cdac953a9f8439038b21612
Souravn1200 <souravn1200@gmail.com>
2023-10-20 17:37:04 +0600

    Fetchig data with brand name daianamicaly

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-//motor
-// jDosTuFhq0T9JUY1
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yctm60s.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,12 +28,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('productDB').collection('products')
+
+    app.get('/products/:brand', async(req, res) => {
+
+      const brand = req.params.brand
+      const query = {brand : brand}
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray()
+     
+      // console.log(brand);
+      res.send(result);
+
+
+    })
+
+
+    app.post('/products', async(req, res) => {
+        const newProduct =  req.body
+        console.log(newProduct);
+        const result =  await productCollection.insertOne(newProduct)
+        res.send(result)
+    })
+
+    
+
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
